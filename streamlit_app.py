245c70a3b0010984326c4ba99905d2c89e18e976
Devmurthy <nklnm2005@gmail.com>
2026-01-18 15:42:49 +0530

    Add navigation menu items to navbar matching index.html

--- streamlit_app.py
+++ streamlit_app.py
@@ -629,6 +629,13 @@ nav_html = """
                 <span class="nav-brand-subtitle">Automated Web Testing</span>
             </div>
         </div>
+        <ul class="nav-menu">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#features">Features</a></li>
+            <li><a href="#how-it-works">How It Works</a></li>
+            <li><a href="#demo">Try It</a></li>
+        </ul>
     </div>
 </div>
 """
